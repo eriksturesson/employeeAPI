68ceee50da8fdd23107d1a4062def92d00bbb14c
Erik Sturesson <hej@eriksturesson.se>
2025-05-15 11:53:59 +0200

    First choosen architecture on place, no code at all

---
+++ readme.md
@@ -0,0 +1,9 @@
+# An API for handling Employees
+
+## Features are:
+
+1. Add a new employee.
+
+2. Remove an employee.
+
+3. Fetch all employees.
---
+++ src/tests/employee.test.ts
@@ -0,0 +1,7 @@
+// src/tests/dummy.test.ts
+describe("Dummy test", () => {
+  it("adds two numbers correctly", () => {
+    const sum = 2 + 3;
+    expect(sum).toBe(5);
+  });
+});
